19c4e4a5becd07272d1fe2651016bb9027d1d3d4
Ciara <ciarafumar@gmail.com>
2023-03-16 13:31:19 -0700

    added linkedin links

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,19 +6,33 @@ function Footer() {
         <div className='everyones-information-container'>
             <div className='le-div'>
                 <p className='name'>Le Nguyen</p>
-                <a className='github-button' href='https://github.com/ltnguyen517'><i class="fa-brands fa-github fa-1x"></i></a>
+                <div className='link-div'>
+                    <a className='github-button' href='https://github.com/ltnguyen517'><i class="fa-brands fa-github fa-1x"></i></a>
+                    <a className='linkedin-button' href='https://www.linkedin.com/in/lenguyenmbs/' style={{fontFamily: 'Helvetica'}}><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+
             </div>
             <div className='sarah-div'>
                 <p className='name'>Sarah Ling</p>
-                <a className='github-button' href='https://github.com/lingxiGitHub'><i class="fa-brands fa-github fa-1x"></i></a>
+                <div className='link-div'>
+                    <a className='github-button' href='https://github.com/lingxiGitHub'><i class="fa-brands fa-github fa-1x"></i></a>
+                    <a className='linkedin-button' href='https://www.linkedin.com/in/xi-ling-cpa/' style={{fontFamily: 'Helvetica'}}><i class="fa-brands fa-linkedin"></i></a>
+                </div>
             </div>
             <div className='ciara-div'>
                 <p className='name'>Ciara Fumar Christensen</p>
-                <a className='github-button' href='https://github.com/c-iarraa'><i class="fa-brands fa-github fa-1x"></i></a>
+                <div className='link-div'>
+                    <a className='github-button' href='https://github.com/c-iarraa'><i class="fa-brands fa-github fa-1x"></i></a>
+                    <a className='linkedin-button' href='https://www.linkedin.com/in/ciara-fumar-christensen-913972268/' style={{fontFamily: 'Helvetica'}}><i class="fa-brands fa-linkedin"></i></a>
+                </div>
             </div>
             <div className='xiao-div'>
                 <p className='name'>Xiaoguang Wang</p>
-                <a className='github-button' href='https://github.com/guangxiaozhi'><i class="fa-brands fa-github fa-1x"></i></a>
+                <div className='link-div'>
+                    <a className='github-button' href='https://github.com/guangxiaozhi'><i class="fa-brands fa-github fa-1x"></i></a>
+                    <a className='linkedin-button' href='https://www.linkedin.com/in/xiaoguang-wang-096128265/' style={{fontFamily: 'Helvetica'}}><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+
             </div>
         </div>
     )
